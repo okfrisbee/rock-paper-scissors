8aec0ffafdcc8738f0e37e227edbd1e8eecd76c1
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-02-12 14:51:47 -0800

    Create playRound function to simulate a RPS game

--- javascript.js
+++ javascript.js
@@ -13,3 +13,39 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Rock, Paper, or Scissors? ").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "rock") {
+        console.log("Tie!")
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats rock.");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie!");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Tie!");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
