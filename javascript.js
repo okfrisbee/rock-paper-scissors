5768e62721aa925b38d38bae86d8baa8e7d5cc25
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-02-12 14:32:54 -0800

    Create getComputerChoice function to return choice

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    switch (randomNum) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
